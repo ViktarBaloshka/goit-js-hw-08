01bc854e493c2b47f58bbc420b9392383144c9d3
ViktarBaloshka <vbolosko@gmail.com>
2022-05-04 13:02:16 +0200

    JS-HW-8-START

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createListImages(galleryItems);
 
-console.log(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createListImages(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+  <li> 
+    <a class="gallery__item" href="${original}">
+        <img
+            style="display:block"
+            class="gallery__image"
+            src="${preview}" 
+            alt="${description}"
+        />
+    </a>
+  </li>
+    `;
+    })
+    .join('');
+}
+new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
